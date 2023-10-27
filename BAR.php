bf985a58e1132f1f237ed227e958cb77f81bb6e9
SLafrance69 <slafra@lacitec.on.ca>
2023-10-27 14:42:33 -0400

    Update BAR.php

--- BAR.php
+++ BAR.php
@@ -2,6 +2,23 @@
 	$ScenarioId=$_GET['ScenarioId'];
 ?>
 
+<?php
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:landis.database.windows.net,1433; Database = landis", "sa.admin", "Voma!495999!");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+//$connectionInfo = array("UID" => "sa.admin", "pwd" => "{your_password_here}", "Database" => "landis", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+//$serverName = "tcp:landis.database.windows.net,1433";
+//$conn = sqlsrv_connect($serverName, $connectionInfo);
+?>
+
 <!doctype html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
