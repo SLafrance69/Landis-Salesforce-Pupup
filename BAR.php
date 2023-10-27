59d914bd44f000f20c9acfcbe46fd65476a26067
SLafrance69 <slafra@lacitec.on.ca>
2023-10-27 15:31:49 -0400

    Update BAR.php

--- BAR.php
+++ BAR.php
@@ -13,11 +13,9 @@
     $conn = sqlsrv_connect($serverName, $connectionOptions);
     $tsql= "SELECT [ID],[ScenarioId],[CallerNumber],[CallerName],[StudentID],[ContactID] FROM [dbo].[IVR-BAR]";
     $getResults= sqlsrv_query($conn, $tsql);
-    echo ("Reading data from table" . PHP_EOL);
-    if ($getResults == FALSE)
-        echo (sqlsrv_errors());
-    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
-     echo ($row['ID'] . " " . $row['ScenarioId']. " " . $row['CallerNumber']. " " . $row['CallerName']. " " . $row['StudentID']. " " . $row['ContactID'] . PHP_EOL);
+
+	while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+     echo ($row['ID'] . " " . $row['ScenarioId']. " " . $row['CallerNumber']. " " . $row['CallerName']. " " . $row['StudentID']. " " . $row['ContactID'] . "/n");
     }
     sqlsrv_free_stmt($getResults);
 ?>
