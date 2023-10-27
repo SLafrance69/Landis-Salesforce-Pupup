6c572e1e4f2179a947378d5e5897db1687a4b7ac
SLafrance69 <slafra@lacitec.on.ca>
2023-10-27 14:32:07 -0400

    Update BAR.php

--- BAR.php
+++ BAR.php
@@ -1,10 +1,17 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
-</head>
+<?PHP
+	$ScenarioId=$_GET['ScenarioId'];
+?>
 
+<!doctype html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-Equiv="Cache-Control" Content="no-cache">
+<meta http-Equiv="Pragma" Content="no-cache">
+<meta http-Equiv="Expires" Content="0">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Landis-Salesforce-Popup</title>
+</head>
 <body>
+	<?PHP echo $ScenarioId; ?>
 </body>
 </html>
