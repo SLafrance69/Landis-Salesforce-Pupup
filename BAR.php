7f651fb8800b6e8425d8e20234f94212364f764e
SLafrance69 <slafra@lacitec.on.ca>
2023-10-27 16:05:06 -0400

    Update BAR.php

--- BAR.php
+++ BAR.php
@@ -11,11 +11,17 @@
     );
     //Establishes the connection
     $conn = sqlsrv_connect($serverName, $connectionOptions);
-    $tsql= "SELECT [ID],[ScenarioId],[CallerNumber],[CallerName],[StudentID],[ContactID] FROM [dbo].[IVR-BAR]";
+    $tsql= "SELECT [ID],[ScenarioId],[CallerNumber],[CallerName],[StudentID],[ContactID] FROM [dbo].[IVR-BAR] WHERE [dbo].[IVR-BAR].ScenarioId like '"+ $ScenarioId +"' ";
     $getResults= sqlsrv_query($conn, $tsql);
 
-	while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
-     echo ($row['ID'] . " " . $row['ScenarioId']. " " . $row['CallerNumber']. " " . $row['CallerName']. " " . $row['StudentID']. " " . $row['ContactID'] . "/n");
+	$Count=0;
+	while ($Info = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+		$CallerNumber		= $Info["CallerNumber"];
+		$CallerName			= $Info["CallerName"];
+		$StudentID			= $Info["StudentID"];
+		$ContactID[$Count]	= $Info["ContactID"];
+		
+		$Count++;
     }
     sqlsrv_free_stmt($getResults);
 ?>
@@ -28,8 +34,40 @@
 <meta http-Equiv="Expires" Content="0">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Landis-Salesforce-Popup</title>
+<style type="text/css">
+body {
+    background-color: #FFFFFF;
+}
+</style>
 </head>
 <body>
-	<?PHP echo $ScenarioId; ?>
+	<table width="95%" border="4" align="center">
+	  <tbody>
+	    <tr>
+	      <td align="center" valign="middle" nowrap="nowrap" bgcolor="#000000" style="color: #FFFFFF; font-size: 24px; font-weight: bold;">Landis - Salesforce</td>
+        </tr>
+      </tbody>
+	</table>
+	<table width="90%" border="2" align="center">
+	  <tbody>
+	    <tr>
+	      <td width="25%" align="right" valign="middle" bgcolor="#AAAAAA" style="font-size: 18px">ScenarioId</td>
+	      <td align="left" valign="middle" bgcolor="#FFFFFF" style="color: #FFFFFF"><?PHP echo $ScenarioId; ?></td>
+        </tr>
+	    <tr>
+	      <td width="25%" align="right" valign="middle" bgcolor="#AAAAAA" style="font-size: 18px">CallerNumber</td>
+	      <td align="left" valign="middle" bgcolor="#FFFFFF" style="color: #FFFFFF"><?PHP echo $CallerNumber; ?></td>
+        </tr>
+	    <tr>
+	      <td width="25%" align="right" valign="middle" bgcolor="#AAAAAA" style="font-size: 18px">CallerName</td>
+	      <td align="left" valign="middle" bgcolor="#FFFFFF" style="color: #FFFFFF"><?PHP echo $CallerName; ?></td>
+        </tr>
+	    <tr>
+	      <td width="25%" align="right" valign="middle" bgcolor="#AAAAAA" style="font-size: 18px">StudentID</td>
+	      <td align="left" valign="middle" bgcolor="#FFFFFF" style="color: #FFFFFF"><?PHP echo $StudentID; ?></td>
+        </tr>
+      </tbody>
+</table>
+	<p>&nbsp;</p>
 </body>
 </html>
