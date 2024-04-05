f7fe3ff801f7f25103a9e4a8b7ed738772a1f383
SLafrance69 <91984201+SLafrance69@users.noreply.github.com>
2024-04-05 12:26:31 -0400

    Create sql_test.php

---
+++ sql_test.php
@@ -0,0 +1,16 @@
+<?php
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:sql-landis.database.windows.net,1433; Database = Landis", "sa.local", "L3tM3!nSQL2024");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+//$connectionInfo = array("UID" => "sa.local", "pwd" => "L3tM3!nSQL2024", "Database" => "Landis", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+//$serverName = "tcp:sql-landis.database.windows.net,1433";
+//$conn = sqlsrv_connect($serverName, $connectionInfo);
+?>
