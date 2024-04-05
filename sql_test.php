4d40636b2d20b2f8e3d89d4a8200702bb802b481
SLafrance69 <91984201+SLafrance69@users.noreply.github.com>
2024-04-05 12:28:14 -0400

    Update sql_test.php

--- sql_test.php
+++ sql_test.php
@@ -1,4 +1,7 @@
 <?php
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
 // PHP Data Objects(PDO) Sample Code:
 try {
     $conn = new PDO("sqlsrv:server = tcp:sql-landis.database.windows.net,1433; Database = Landis", "sa.local", "L3tM3!nSQL2024");
