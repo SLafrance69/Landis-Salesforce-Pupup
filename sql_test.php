ca0b201bc7a6285d79012379da10ee643e8151fd
SLafrance69 <91984201+SLafrance69@users.noreply.github.com>
2024-04-05 14:14:14 -0400

    Update sql_test.php

--- sql_test.php
+++ sql_test.php
@@ -3,26 +3,17 @@
 	ini_set('display_startup_errors', 1);
 	error_reporting(E_ALL);
 
-	// SQL Server Extension Sample Code:
-	$connectionInfo = array("UID" => "sa.local", "pwd" => "L3tM3!nSQL2024", "Database" => "Landis", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 1);
-	$serverName = "tcp:sql-landis.database.windows.net,1433";
-	$conn = sqlsrv_connect($serverName, $connectionInfo);
-		if( $conn === false ) {
-     			die( print_r( sqlsrv_errors(), true));
-		}else{
-			print_r("Connected");
-		}
+	try {
+    		$conn = new PDO("sqlsrv:server = tcp:sql-landis.database.windows.net,1433; Database = Landis", "sa.local", "L3tM3!nSQL2024");
+   		 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	}
+	catch (PDOException $e) {
+    		print("Error connecting to SQL Server.");
+    		die(print_r($e));
+	}
 
-	$sql= "SELECT * FROM BAR"; // WHERE ScenarioId like '$ScenarioId' ";
-    	$stmt = sqlsrv_query($conn, $sql);
-		if( $stmt === false) {
-    			die( print_r( sqlsrv_errors(), true) );
-		}
-
-	$rows_affected = sqlsrv_rows_affected( $stmt);
-	echo "[".$rows_affected."]<br>";
-	while ($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
-		echo $row['ScenarioId']." ; ".$row['CallerNumber']." ; ".$row['CallerName']." ; ".$row['StudentID']." ; ".$row['ContactID']." <br> ";
-    	}
-    	sqlsrv_free_stmt($stmt);
+	$data = $conn->query("SELECT * FROM BAR")->fetchAll();
+	foreach ($data as $row) {
+   		echo $row['ScenarioId']." ; ".$row['CallerNumber']." ; ".$row['CallerName']." ; ".$row['StudentID']." ; ".$row['ContactID']."<br />\n";
+	}
 ?>
