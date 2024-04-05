538c55682bede94f9ca377f88297203f08ee385b
SLafrance69 <91984201+SLafrance69@users.noreply.github.com>
2024-04-05 09:46:27 -0400

    Update popup.php

--- popup.php
+++ popup.php
@@ -10,20 +10,20 @@
         "PWD" => "Voma!495999!"
     );
     //Establishes the connection
-    $conn = sqlsrv_connect($serverName, $connectionOptions);
-    $tsql= "SELECT [ID],[ScenarioId],[CallerNumber],[CallerName],[StudentID],[ContactID] FROM [dbo].[IVR] WHERE [dbo].[IVR].ScenarioId like '".$ScenarioId."' ";
-    $getResults= sqlsrv_query($conn, $tsql);
+//    $conn = sqlsrv_connect($serverName, $connectionOptions);
+//    $tsql= "SELECT [ID],[ScenarioId],[CallerNumber],[CallerName],[StudentID],[ContactID] FROM [dbo].[IVR] WHERE [dbo].[IVR].ScenarioId like '".$ScenarioId."' ";
+//    $getResults= sqlsrv_query($conn, $tsql);
 
-	$Count=0;
-	while ($Info = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
-		$CallerNumber		= $Info["CallerNumber"];
-		$CallerName			= $Info["CallerName"];
-		$StudentID			= $Info["StudentID"];
-		$ContactID[$Count]	= $Info["ContactID"];
+//	$Count=0;
+//	while ($Info = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+//		$CallerNumber		= $Info["CallerNumber"];
+//		$CallerName			= $Info["CallerName"];
+//		$StudentID			= $Info["StudentID"];
+//		$ContactID[$Count]	= $Info["ContactID"];
 		
-		$Count++;
-    }
-    sqlsrv_free_stmt($getResults);
+//		$Count++;
+//    }
+//    sqlsrv_free_stmt($getResults);
 ?>
 
 <!doctype html>
@@ -89,7 +89,5 @@ body {
 		  <?PHP endfor; ?>
       </tbody>
 </table>
-	<p>&nbsp;</p>
-	<p>&nbsp;</p>
 </body>
 </html>
