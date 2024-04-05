067b5efdff0b856641a7315b9615541c092981fd
SLafrance69 <91984201+SLafrance69@users.noreply.github.com>
2024-04-05 13:40:01 -0400

    Update sql_test.php

--- sql_test.php
+++ sql_test.php
@@ -2,22 +2,33 @@
 	ini_set('display_errors', 1);
 	ini_set('display_startup_errors', 1);
 	error_reporting(E_ALL);
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:sql-landis.database.windows.net,1433; Database = Landis", "sa.local", "L3tM3!nSQL2024");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
 
-print("PASS Connection Point A<BR>");
+	// SQL Server Extension Sample Code:
+	$connectionInfo = array("UID" => "sa.local", "pwd" => "L3tM3!nSQL2024", "Database" => "Landis", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+	$serverName = "tcp:sql-landis.database.windows.net,1433";
+	$conn = sqlsrv_connect($serverName, $connectionInfo);
+		if( $conn === false ) {
+     			die( print_r( sqlsrv_errors(), true));
+		}else{
+			print_r("Connected");
+		}
 
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "sa.local", "pwd" => "L3tM3!nSQL2024", "Database" => "Landis", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:sql-landis.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
+	$sql= "SELECT * FROM BAR" // WHERE ScenarioId like '$ScenarioId' ";
+    	$stmt = sqlsrv_query($conn, $sql);
+		if( $stmt === false) {
+    			die( print_r( sqlsrv_errors(), true) );
+		}
 
-print("PASS Connection Point B<BR>");
+	while ($Info = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
+		$ScenarioId		= $Info['ScenarioId'];
+		$CallerNumber		= $Info['CallerNumber'];
+		$CallerName		= $Info['CallerName'];
+		$StudentID		= $Info['StudentID'];
+		$ContactID		= $Info['ContactID'];
+
+		$MSG = "[".$ScenarioId."]"." "."[".$CallerNumber."]"." "."[".$CallerName."]"." "."[".$StudentID."]"." "."[".$ContactID."]";
+
+		print_r($MSG);
+    	}
+    	sqlsrv_free_stmt($getResults);
 ?>
