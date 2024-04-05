44e8c4cc6faf5b8a4a6d9c4455b6db60413677cc
SLafrance69 <91984201+SLafrance69@users.noreply.github.com>
2024-04-05 13:02:39 -0400

    Update popup.php

--- popup.php
+++ popup.php
@@ -7,7 +7,7 @@
 
 	//Establishes the connection
 	$connectionInfo = array("UID" => "sa.local", "pwd" => "L3tM3!nSQL2024", "Database" => "Landis", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-	$serverName = "tcp:sql-landis.database.windows.net,1433";
+	$serverName = "sql-landis.database.windows.net\Landis";
 	$conn = sqlsrv_connect($serverName, $connectionInfo);
 
 	$tsql= "SELECT * FROM [dbo].[BAR] WHERE [dbo].[BAR].ScenarioId like '".$ScenarioId."' ";
